f3ee0cb611bfea301da81694f2b9e3cb7c918f8d
Tala <slemantala@example.com>
2024-03-30 12:36:13 -0400

    implement cat-file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 import sys
 import os
-
+import zlib
+import re
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     #
@@ -16,8 +15,20 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
-    else:
+    elif command == "cat-file":
+        option = sys.argv[2]
+        file = sys.argv[3]
+        if option == '-p':
+            with open(f".git/objects/{file[:2]}/{file[2:]}", 'rb') as file:
+                content = file.read()
+                
+            decompressed_content = zlib.decompress(content)
+            text_data = decompressed_content.decode('utf-8')
+            filtered_data = re.sub(r'^.*?\x00', '', text_data)
+            print(filtered_data, end='')
+    else :
         raise RuntimeError(f"Unknown command #{command}")
+    
 
 
 if __name__ == "__main__":
